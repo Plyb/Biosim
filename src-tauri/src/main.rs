88f380307607ba58c54b99125b3f792d50919722
Koby Lewis <kobyjlewis@gmail.com>
2024-05-05 13:09:35 -0600

    remove unused tutorial code

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,33 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::{f32::consts::PI, thread, time};
+use std::f32::consts::PI;
 use arr_macro::arr;
 use bevy::{
-  core_pipeline::core_2d::Transparent2d,
   prelude::*,
   render::{
-      mesh::{Indices, MeshVertexAttribute},
-      render_asset::{RenderAssetUsages, RenderAssets},
-      render_phase::{AddRenderCommand, DrawFunctions, RenderPhase, SetItemPipeline},
-      render_resource::{
-          BlendState, ColorTargetState, ColorWrites, Face, FragmentState, FrontFace,
-          MultisampleState, PipelineCache, PolygonMode, PrimitiveState, PrimitiveTopology,
-          PushConstantRange, RenderPipelineDescriptor, ShaderStages, SpecializedRenderPipeline,
-          SpecializedRenderPipelines, TextureFormat, VertexBufferLayout, VertexFormat,
-          VertexState, VertexStepMode,
-      },
-      texture::BevyDefault,
-      view::{ExtractedView, ViewTarget, VisibleEntities},
-      Extract, Render, RenderApp, RenderSet,
+      mesh::Indices,
+      render_asset::RenderAssetUsages,
+      render_resource::PrimitiveTopology,
   },
   sprite::{
-      extract_mesh2d, DrawMesh2d, Material2dBindGroupId, MaterialMesh2dBundle, Mesh2d, Mesh2dHandle, Mesh2dPipeline, Mesh2dPipelineKey, Mesh2dRenderPlugin, Mesh2dTransforms, MeshFlags, RenderMesh2dInstance, RenderMesh2dInstances, SetMesh2dBindGroup, SetMesh2dViewBindGroup
+      MaterialMesh2dBundle, Mesh2dHandle
   },
-  utils::FloatOrd,
 };
 
 use bevy_pancam::{PanCam, PanCamPlugin};
-use tauri::{LogicalSize, Manager, Size, Window};
 use world::{Cell, World, WORLD_WIDTH};
 
 mod world;
@@ -36,22 +23,6 @@ fn main() {
   App::new()
     .add_plugins((DefaultPlugins, HelloPlugin))
     .run();
-  // tauri::Builder::default()
-  //   .setup(|app| {
-  //     let main_window = app.get_window("main").unwrap();
-  //     let _ = main_window.set_size(
-  //       Size::Logical(LogicalSize { width: 500.0, height: 500.0})
-  //     );
-
-  //     thread::spawn(move || {
-  //       start_simulation(main_window)
-  //     });
-
-  //     Ok(())
-  //   })
-  //   .invoke_handler(tauri::generate_handler![get_world_width])
-  //   .run(tauri::generate_context!())
-  //   .expect("error while running tauri application");
 }
 
 pub struct HelloPlugin;
@@ -59,24 +30,18 @@ pub struct HelloPlugin;
 impl Plugin for HelloPlugin {
   fn build(&self, app: &mut App) {
     app.add_plugins(PanCamPlugin::default())
-      .insert_resource(GreetTimer(Timer::from_seconds(0.5, TimerMode::Repeating)))
+      .insert_resource(WorldTickTimer(Timer::from_seconds(0.5, TimerMode::Repeating)))
       .add_systems(Startup, setup)
       .add_systems(Update, update_world);
   }
 }
 
-#[derive(Component)]
-struct Person;
-
-#[derive(Component)]
-struct Name(String);
-
 #[derive(Resource)]
-struct GreetTimer(Timer);
+struct WorldTickTimer(Timer);
 
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
   commands.spawn(Camera2dBundle::default())
-    .insert(PanCam::default());;
+    .insert(PanCam::default());
 
   commands.spawn(MaterialMesh2dBundle {
     mesh: meshes.add(build_hex_grid_mesh()).into(),
@@ -150,26 +115,12 @@ fn get_indices() -> Vec<u32> {
   indices
 }
 
-fn add_people(mut commands: Commands) {
-  commands.spawn((Person, Name("Elaina Proctor".to_string())));
-  commands.spawn((Person, Name("Renzo Hume".to_string())));
-  commands.spawn((Person, Name("Zayna Nieves".to_string())));
-}
-
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
-  if timer.0.tick(time.delta()).just_finished() {
-    for name in &query {
-        println!("hello {}!", name.0);
-    }
-  }
-}
-
-fn update_world(mut meshes: ResMut<Assets<Mesh>>, mut timer: ResMut<GreetTimer>, time: Res<Time>, mut query: Query<(&mut WorldComponent, &mut Mesh2dHandle)>) {
+fn update_world(mut meshes: ResMut<Assets<Mesh>>, mut timer: ResMut<WorldTickTimer>, time: Res<Time>, mut query: Query<(&mut WorldComponent, &mut Mesh2dHandle)>) {
   if !timer.0.tick(time.delta()).just_finished() {
     return;
   }
 
-  for (mut world_component, mut mesh_handle) in &mut query {
+  for (mut world_component, mesh_handle) in &mut query {
     let Some(mesh) = meshes.get_mut(mesh_handle.0.id()) else {
       break;
     };
@@ -183,28 +134,4 @@ fn update_world(mut meshes: ResMut<Assets<Mesh>>, mut timer: ResMut<GreetTimer>,
 
     world_component.0 = world_component.0.tick();
   }
-}
-
-fn update_people(mut query: Query<&mut Name, With<Person>>) {
-  for mut name in &mut query {
-      if name.0 == "Elaina Proctor" {
-          name.0 = "Elaina Hume".to_string();
-          break; // We don’t need to change any other names
-      }
-  }
-}
-
-fn start_simulation(window : Window) {
-  let mut world = World::new_random();
-
-  loop {
-    window.emit("update-world", &world).unwrap();
-    world = world.tick();
-    thread::sleep(time::Duration::from_millis(500));
-  }
-}
-
-#[tauri::command]
-fn get_world_width() -> usize {
-  WORLD_WIDTH
 }
