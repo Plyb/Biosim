a64b4a83a93c4ca9124013787747414e2c5ff872
Koby Lewis <kobyjlewis@gmail.com>
2024-05-04 21:46:49 -0600

    translate hex mesh algorithm to rust

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -73,50 +73,74 @@ struct GreetTimer(Timer);
 fn setup(mut commands: Commands, mut meshes: ResMut<Assets<Mesh>>, mut materials: ResMut<Assets<ColorMaterial>>) {
   commands.spawn(Camera2dBundle::default());
 
-  let mut star = Mesh::new(
+  commands.spawn(MaterialMesh2dBundle {
+    mesh: meshes.add(build_hex_grid_mesh()).into(),
+    material: materials.add(Color::WHITE),
+    ..default()
+  });
+}
+
+
+const NUM_VERTS_PER_HEX: u32 = 6;
+fn build_hex_grid_mesh() -> Mesh {
+  let mut mesh = Mesh::new(
     PrimitiveTopology::TriangleList,
-    RenderAssetUsages::RENDER_WORLD,
+     RenderAssetUsages::RENDER_WORLD
+  );
+  mesh.insert_attribute(Mesh::ATTRIBUTE_POSITION, get_positions());
+  mesh.insert_indices(Indices::U32(get_indices()));
+  mesh.insert_attribute(
+    Mesh::ATTRIBUTE_COLOR, 
+    vec![
+      Color::WHITE.as_linear_rgba_f32();
+      WORLD_WIDTH * WORLD_WIDTH * NUM_VERTS_PER_HEX as usize
+    ]
   );
 
-  let mut v_pos = vec![[0.0, 0.0, 0.0]];
-    for i in 0..10 {
-        // The angle between each vertex is 1/10 of a full rotation.
-        let a = i as f32 * PI / 5.0;
-        // The radius of inner vertices (even indices) is 100. For outer vertices (odd indices) it's 200.
-        let r = (1 - i % 2) as f32 * 100.0 + 100.0;
-        // Add the vertex position.
-        v_pos.push([r * a.sin(), r * a.cos(), 0.0]);
+  mesh
+}
+
+fn get_positions() -> Vec<[f32; 3]> {
+  let mut positions: Vec<[f32; 3]> = vec![];
+
+  let delta_y = (3.0f32).sqrt() / 2.;
+  for x in 0..WORLD_WIDTH {
+    for y in 0..WORLD_WIDTH {
+      let x_center = (3. * x as f32) + (3. * (y as f32 / 2.));
+      let y_center = delta_y * y as f32;
+
+      for v in 0..6 {
+        positions.push(get_hex_vertex_pos(x_center, y_center, v))
+      }
     }
-    // Set the position attribute
-    star.insert_attribute(Mesh::ATTRIBUTE_POSITION, v_pos);
-    // And a RGB color attribute as well
-    let mut v_color: Vec<[f32; 4]> = vec![Color::BLACK.as_linear_rgba_f32()];
-    v_color.extend_from_slice(&[Color::YELLOW.as_linear_rgba_f32(); 10]);
-    star.insert_attribute(
-        Mesh::ATTRIBUTE_COLOR,
-        v_color,
-    );
+  }
 
-    let mut indices = vec![0, 1, 10];
-    for i in 2..=10 {
-        indices.extend_from_slice(&[0, i, i - 1]);
+  positions
+}
+
+fn get_hex_vertex_pos(x_center: f32, y_center: f32, vertex_index: usize) -> [f32; 3] {
+  const ANGLE_MULTIPLIER: f32 = PI / 3.;
+
+  let angle = ANGLE_MULTIPLIER * vertex_index as f32;
+  [angle.cos() + x_center, angle.sin() + y_center, 0.]
+}
+
+fn get_indices() -> Vec<u32> {
+  let mut indices: Vec<u32> = vec![];
+
+  for x in 0..WORLD_WIDTH {
+    for y in 0..WORLD_WIDTH {
+      let offset = (x as u32 + (y * WORLD_WIDTH) as u32) * NUM_VERTS_PER_HEX;
+      indices.extend_from_slice(&[
+        offset + 0, offset + 1, offset + 5,
+        offset + 1, offset + 2, offset + 5,
+        offset + 2, offset + 4, offset + 5,
+        offset + 2, offset + 3, offset + 4,
+      ]);
     }
-    star.insert_indices(Indices::U32(indices));
+  }
 
-    commands.spawn(MaterialMesh2dBundle {
-      mesh: meshes.add(star).into(),
-      material: materials.add(Color::WHITE),
-      ..default()
-    });
-
-  //   commands.spawn((
-  //     // We use a marker component to identify the custom colored meshes
-  //     Mesh2d,
-  //     // The `Handle<Mesh>` needs to be wrapped in a `Mesh2dHandle` to use 2d rendering instead of 3d
-  //     Mesh2dHandle(meshes.add(star)),
-  //     // This bundle's components are needed for something to be rendered
-  //     SpatialBundle::INHERITED_IDENTITY,
-  // ));
+  indices
 }
 
 fn add_people(mut commands: Commands) {
