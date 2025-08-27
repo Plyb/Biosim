dc62a1c86819bd16657fa04011cd9831c253aa90
Koby Lewis <kobyjlewis@gmail.com>
2025-08-26 18:12:18 -0600

    comment out unused cpu code

--- app/src/world.rs
+++ app/src/world.rs
@@ -1,4 +1,4 @@
-use biosim_core::{world::{get_index, Cell, WorldCoord, WorldCursor}, WORLD_WIDTH};
+use biosim_core::{world::Cell, WORLD_WIDTH};
 
 pub fn new_random() -> Vec<Cell> {
     let mut cells: Vec<Cell> = Vec::with_capacity(WORLD_WIDTH * WORLD_WIDTH);
@@ -10,15 +10,15 @@ pub fn new_random() -> Vec<Cell> {
     cells
 }
 
-pub fn tick(cells: &Vec<Cell>) -> Vec<Cell> {
-    let mut new_cells : Vec<Cell> = vec![Cell::Dead; WORLD_WIDTH * WORLD_WIDTH];
+// pub fn tick(cells: &Vec<Cell>) -> Vec<Cell> {
+//     let mut new_cells : Vec<Cell> = vec![Cell::Dead; WORLD_WIDTH * WORLD_WIDTH];
 
-    for x in 0..WORLD_WIDTH {
-        for y in 0..WORLD_WIDTH {
-            let coord = WorldCoord { x, y };
-            let cursor = WorldCursor::new(cells.as_slice().try_into().unwrap(), coord);
-            new_cells[get_index(coord)] = cursor.get_new_state();
-        }
-    }
-    new_cells
-}
+//     for x in 0..WORLD_WIDTH {
+//         for y in 0..WORLD_WIDTH {
+//             let coord = WorldCoord { x, y };
+//             let cursor = WorldCursor::new(cells.as_slice().try_into().unwrap(), coord);
+//             new_cells[get_index(coord)] = cursor.get_new_state();
+//         }
+//     }
+//     new_cells
+// }
